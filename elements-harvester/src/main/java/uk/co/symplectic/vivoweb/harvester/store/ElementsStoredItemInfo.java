c2e102f182d1efe217c7cd5c70e284ff4b94113f
ajpc2 <andrew@symplectic.co.uk>
2019-09-27 17:41:40 +0100

    Update Copyright to 2019

--- elements-harvester/src/main/java/uk/co/symplectic/vivoweb/harvester/store/ElementsStoredItemInfo.java
+++ elements-harvester/src/main/java/uk/co/symplectic/vivoweb/harvester/store/ElementsStoredItemInfo.java
@@ -1,9 +1,11 @@
 /*
  * ******************************************************************************
- *   Copyright (c) 2017 Symplectic. All rights reserved.
+ *   Copyright (c) 2019 Symplectic. All rights reserved.
  *   This Source Code Form is subject to the terms of the Mozilla Public
  *   License, v. 2.0. If a copy of the MPL was not distributed with this
  *   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * ******************************************************************************
+ *   Version :  ${git.branch}:${git.commit.id}
  * ******************************************************************************
  */
 
